51f466a92fc4ffc14da832f2065b8bc72a38a7b9
Manoel Horta Ribeiro <manoelhortaribeiro@gmail.com>
2017-10-26 10:36:22 -0200

    Fixes a bunch of small bugs and makes the data in tweets richer.

--- AbusiveUsersOSNs/rwwj_crawler.py
+++ AbusiveUsersOSNs/rwwj_crawler.py
@@ -1,6 +1,6 @@
+from AbusiveUsersOSNs.neo4j_ogm_schema import tweepy2neo4j_tweet, tweepy2neo4j_materialize_user, tweepy2string_tweet
 from AbusiveUsersOSNs.neo4j_ogm_schema import tweepy2neo4j_user, tweepy2neo4j_virtual_user, tweepy2neo4j_media
-from AbusiveUsersOSNs.neo4j_ogm_schema import tweepy2neo4j_tweet, tweepy2neo4j_materialize_user
-from py2neo import Graph, Relationship, Node, NodeSelector
+from py2neo import Graph, Relationship, NodeSelector
 from urllib.parse import urlparse
 from datetime import datetime
 import random
@@ -17,7 +17,9 @@ class MHCrawler:
         self.graph = MHCrawler.auth_neo4j(auth_neo4j)
         self.node_selector = NodeSelector(self.graph)
         self.seed, self.n, self.w = seed, n, w
-        self.next_node = Node("User", **next_node) if next_node is not None else next_node
+        self.next_node = list(self.node_selector.select("User", id=next_node["id"]))[0] \
+            if next_node is not None else next_node
+        self.previous_node = None
 
         if self.graph.schema.get_uniqueness_constraints("User") != ["id"]:
             self.graph.schema.create_uniqueness_constraint("User", "id")
@@ -30,6 +32,24 @@ class MHCrawler:
 
         if "n" not in self.graph.schema.get_indexes("User"):
             self.graph.schema.create_index("User", "n")
+
+    @staticmethod
+    def control_break():
+        g = open("control.json", 'r')
+        control_var = json.load(g)
+        g.close()
+        if not control_var["continue"]:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def control_save(next_node, cont, seed, n):
+        tmp = dict()
+        tmp["next"], tmp["continue"], tmp["seed"], tmp["n"] = next_node, cont, seed, n
+        g = open("control.json", 'w')
+        json.dump(tmp, g)
+        g.close()
 
     @staticmethod
     def auth_tweepy(auth):
@@ -53,6 +73,20 @@ class MHCrawler:
             return [status._json["retweeted_status"]["user"]["id"]]
         return []
 
+    @staticmethod
+    def get_urls(status):
+
+        if 'urls' in status.entities:
+            urls = []
+            for url in status.entities['urls']:
+                if 'expanded_url' in url and url['expanded_url'] is not None:
+                    if urlparse(url['expanded_url']).netloc != "twitter.com":
+                        tmp = urlparse(url["expanded_url"])
+                        urls.append((tmp.netloc + tmp.path, tmp.netloc, tmp.path))
+            return urls
+        else:
+            return []
+
     def get_tweets(self, user_p):
 
         tl = self.api.user_timeline(include_rts=True, count=200, trim_user=False, exclude_replies=False,
@@ -61,24 +95,19 @@ class MHCrawler:
         tweets, urls, quoted, retweeted = [], [], [], []
 
         for tweet in tl:
-            tweets.append("{0},{1},{2}".format(tweet.id, tweet.full_text, tweet.created_at.timestamp()))
-
-            if 'urls' in tweet.entities:
-                for url in tweet.entities['urls']:
-                    if 'expanded_url' in url and url['expanded_url'] is not None:
-                        if urlparse(url['expanded_url']).netloc != "twitter.com":
-                            urls.append((url['expanded_url'], tweet.id))
-
+            tweets.append(tweepy2string_tweet(tweet))
+            urls += MHCrawler.get_urls(tweet)
             quoted += MHCrawler.get_quoted_users(tweet)
             retweeted += MHCrawler.get_retweeted_users(tweet)
 
+        print(tweets[0])
+        print(urls[0])
         tweet_p = tweepy2neo4j_tweet(tweets)
-
         self.graph.create(tweet_p)
         self.graph.create(Relationship(user_p, "tweeted", tweet_p))
 
         for url in urls:
-            url_p = tweepy2neo4j_media(url[0], url[1])
+            url_p = tweepy2neo4j_media(url)
             self.graph.merge(url_p, "Media", "url")
             self.graph.create(Relationship(user_p, "shared", url_p))
 
@@ -101,6 +130,10 @@ class MHCrawler:
 
     def push_node(self, node):
         user = self.api.get_user(node["id"])
+
+        if "lang" not in user._json or user._json["lang"] != "en":
+            raise IOError("User is not english speaking")
+
         user_p = tweepy2neo4j_materialize_user(node, user, self.n)
         self.n += 1
         self.graph.push(user_p)
@@ -121,66 +154,55 @@ class MHCrawler:
         print("Walk {0}".format(self.next_node["id"]))
 
     def jump(self):
-        self.next_node = list(self.node_selector.select("User", n=random.randint(1, self.n - 1)))
-        self.next_node = self.next_node[0]
+        self.next_node = list(self.node_selector.select("User", n=random.randint(1, self.n - 1)))[0]
         print("Jump {0}".format(self.next_node["screen_name"]))
 
     def get_adj(self):
-        adj = []
-        for rel in self.graph.match(start_node=self.next_node, rel_type="quoted", bidirectional=True):
-            adj.append(rel.end_node())
-        for rel in self.graph.match(start_node=self.next_node, rel_type="retweeted", bidirectional=True):
-            adj.append(rel.end_node())
-        return adj
+        return list(set([rel.end_node() for rel in self.graph.match(start_node=self.next_node,
+                                                                    rel_type="retweeted", bidirectional=True)]))
 
     def run(self):
 
         if self.next_node is None:
-            print("Starting...")
-            print("-" * 100)
+            print("Starting... \n " + "-" * 100)
             self.init_run()
 
         while True:
 
-            t1 = datetime.now()
+            try:
+                ts = datetime.now()
 
-            g = open("control.json", 'r')
-            control_var = json.load(g)
-            g.close()
-            if not control_var["continue"]:
-                break
+                if MHCrawler.control_break():
+                    break
 
-            t2 = datetime.now()
+                if self.next_node["virtual"] == "T":
+                    try:
+                        self.next_node = self.push_node(self.next_node)
+                        print("(Pushed {0}".format(self.next_node["screen_name"]), end=") ")
+                    except tweepy.TweepError as exception:
+                        print(exception)
+                        self.jump()
+                    except IOError as exception:
+                        print(exception)
+                        self.next_node = self.previous_node
+                        continue
+                else:
+                    print("(Existed {0}".format(self.next_node["screen_name"]), end=") ")
 
-            if self.next_node["virtual"] == "T":
-                try:
-                    self.next_node = self.push_node(self.next_node)
-                    print("(Pushed {0}".format(self.next_node["screen_name"]), end=") ")
-                except tweepy.TweepError as exception:
-                    print(exception)
-                    self.jump()
-            else:
-                print("(Existed {0}".format(self.next_node["screen_name"]), end=") ")
+                adj = self.get_adj()
+                print(adj)
+                threshold, rand = self.w / (self.w + len(adj)), random.random()
+                self.previous_node = self.next_node
+                self.jump() if rand < threshold else self.walk(adj)
 
-            adj = self.get_adj()
+                print("Iter: {0} \t n:{1} \t tr:{2}".format(datetime.now() - ts, self.n, threshold))
+                print("-" * 100)
 
-            threshold, rand = self.w / (self.w + len(adj)), random.random()
+            except Exception as exception:
+                print(exception)
+                MHCrawler.control_save(self.next_node, True, self.seed, self.n)
 
-            self.jump() if rand < threshold else self.walk(adj)
-
-            t3 = datetime.now()
-
-            print("File management: {0} \t Iter: {1} \t n:{2}".format(t2 - t1, t3 - t2, self.n))
-            print("-" * 100)
-
-        tmp = dict()
-        tmp["next"] = self.next_node
-        tmp["continue"] = True
-        tmp["seed"] = self.seed
-        tmp["n"] = self.n
-        g = open("control.json", 'w')
-        json.dump(tmp, g)
-        g.close()
+        MHCrawler.control_save(self.next_node, True, self.seed, self.n)
 
 
 if __name__ == "__main__":
